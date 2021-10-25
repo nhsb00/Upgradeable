a74c88588bd93f9d5f7c3b1ae1dcab92db770b65
nhsb00 <nhsb00@hotmail.com>
2021-10-25 08:17:30 -0400

    footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -12,12 +12,12 @@ class Footer extends React.Component {
                         <h1>Contact</h1>
                         <h3>Soobin Lim</h3>
                         <li>nhsb00@hotmail.com</li>
+                        <li><a href="https://nhsb00.github.io/" target="_blank">Soobin Lim's Portfolio</a></li>
                     </div>
                     <div className="footer-link">
                         <h1>My Sites</h1>
-                        <a href="https://nhsb00.github.io/" target="_blank">Soobin Lim's Portfolio</a>
-                        <a href="https://www.linkedin.com/in/soobin-lim-2a47a2206/" target="_blank"><i className="fab fa-linkedin-in"></i></a>
-                        <a href="https://github.com/nhsb00/Upgradeable" target="_blank"><i className="fab fa-github"></i></a>
+                        <li><a href="https://www.linkedin.com/in/soobin-lim-2a47a2206/" target="_blank"><i className="fab fa-linkedin-in"></i></a></li>
+                        <li><a href="https://github.com/nhsb00/Upgradeable" target="_blank"><i className="fab fa-github"></i></a></li>
                     </div>
                 </div>
             </div>

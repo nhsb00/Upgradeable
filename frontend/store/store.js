ddd274152e96f175d93b3cd29a80e412d2ffec40
nhsb00 <nhsb00@hotmail.com>
2021-10-27 09:48:01 -0400

    removed logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-// import logger from "redux-logger";
+import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/root_reducer";
 
@@ -11,6 +11,6 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const configureStore = (preloadedState = {}) =>
-    createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+    createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 
 export default configureStore;

f5de8f9f9390e80788db215b679b2e5cd5e810d2
nhsb00 <nhsb00@hotmail.com>
2021-11-08 15:03:39 -0500

    title shortened in splash

--- frontend/components/projects/project_index_item.jsx
+++ frontend/components/projects/project_index_item.jsx
@@ -5,8 +5,8 @@ const ProjectIndexItem = props => (
         <li className="projectindexcontainer">
             <div className="projectindexitem">
                 <Link to={`/projects/${props.project.id}`}><img className="projectimg" src={props.project.photoUrl}/></Link>
-            <Link to={`/projects/${props.project.id}`} className="projecttitle">{props.project.title}<a className="projectby"> by <a>{props.project.user.username}</a></a></Link>
-                
+                <Link to={`/projects/${props.project.id}`} className="projecttitle">{props.project.title}</Link>    
+                <a className="projectby"> by <a>{props.project.user.username}</a></a>
             </div>
         </li>
 );
